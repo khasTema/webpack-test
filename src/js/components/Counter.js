2b284b95b18f4d8a3197e70ac84110106af62785
Artem Khasin <akhasin.dev@gmail.com>
2022-02-11 10:07:39 +0200

    counter done and props are pased

--- src/js/components/Counter.js
+++ src/js/components/Counter.js
@@ -1,15 +1,21 @@
 import React, {useState} from "react";
 
-export default function Counter(){
+export default function Counter(props){
     const [count, setCount] = useState(0)
 
     return(
-        <div className="counter--box">
-            <h3 className="count-holder">{count}</h3>
-            <button className="count-btn" onClick={() => setCount(prev => prev +1)}>+</button>
-            <button className="count-btn" 
-                    disabled={count <= 0 ? true : false} 
-                    onClick={() => setCount(prev => prev -1)}>-</button>
-        </div>
+        <>
+            <div className="counter--box">
+                <h3 className="count-holder">{count}</h3>
+                <button className="count-btn" 
+                        disabled={count <= 0 ? true : false} 
+                        onClick={() => setCount(prev => prev -1)}>-</button>
+                <button className="count-btn" onClick={() => setCount(prev => prev +1)}>+</button>
+            </div>
+            <br />
+            <button 
+                className="save-btn"
+                onClick={() => props.saveCount(count)}>SAVE COUNT</button>
+        </>
     )
 }
--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 
 export default function Home(){
-    return <h1>Home Page</h1>
+
+    const [movieData, setMovieData] = useState([])
+    const [movieNumber, setMovieNumber] = useState(1)
+
+    useEffect(() => {
+        fetch(`https://swapi.dev/api/films/${movieNumber}/`)
+            .then(res => res.json())
+            .then(data => setMovieData(data))
+
+    }, [movieNumber])
+
+    return (
+        <>
+            <h2 className="main--title">Some facts about Star Wars Movies</h2>
+            <div className="movie-box">
+                <h3>{movieData.title}</h3>
+                <p>Director: {movieData.director}</p>
+                <p>Producer: {movieData.producer}</p>
+                <p>Release Date: {movieData.release_date}</p>
+                <p>{movieData.opening_crawl}</p>
+            </div>
+            <button className="counter-btn"
+                    onClick={() => setMovieNumber(prev => prev -1)}
+                    disabled={movieNumber === 1}>Prev</button>
+            <button className="counter-btn"
+                    onClick={() => setMovieNumber(prev => prev +1)}
+                    disabled={movieNumber === 6}>Next</button>
+        </>
+    )
 }
+
